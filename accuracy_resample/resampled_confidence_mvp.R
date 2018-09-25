5578da3e3b1ac30b5a998942184ecb6cc18cde2a
KITE\Mitch <mitchell.lyons@unsw.edu.au>
2018-09-25 12:13:47 +1000

    generic shapefile path

--- accuracy_resample/resampled_confidence_mvp.R
+++ accuracy_resample/resampled_confidence_mvp.R
@@ -11,24 +11,24 @@ source("resampled_accuracy_functions.R")
 # load and prep data ------------------------------------------------------
 
 ## HERON
-heron_benthic <- read_dbf(path = "mvp_heron_confidence/mvp_heron_confidence_benthic.dbf", simplify_classes = T)
-heron_geomorphic <- read_dbf("mvp_heron_confidence/mvp_heron_confidence_geomorphic.dbf", simplify_classes = T)
+heron_benthic <- read_dbf(path = "mvp_shapefiles/mvp_heron_confidence_benthic.dbf", simplify_classes = T)
+heron_geomorphic <- read_dbf("mvp_shapefiles/mvp_heron_confidence_geomorphic.dbf", simplify_classes = T)
 
 ## LIGHTHOUSE
-lighthouse_benthic <- read_dbf(path = "mvp_lighthouse_confidence/mvp_lighthouse_confidence_benthic.dbf", simplify_classes = T)
-lighthouse_geomorphic <- read_dbf("mvp_lighthouse_confidence/mvp_lighthouse_confidence_geomorphic.dbf", simplify_classes = T)
+lighthouse_benthic <- read_dbf(path = "mvp_shapefiles/mvp_lighthouse_confidence_benthic.dbf", simplify_classes = T)
+lighthouse_geomorphic <- read_dbf("mvp_shapefiles/mvp_lighthouse_confidence_geomorphic.dbf", simplify_classes = T)
 
 ## MOOREA
-moorea_benthic <- read_dbf(path = "mvp_moorea_confidence/mvp_moorea_confidence_benthic.dbf", simplify_classes = T)
-moorea_geomorphic <- read_dbf("mvp_moorea_confidence/mvp_moorea_confidence_geomorphic.dbf", simplify_classes = T)
+moorea_benthic <- read_dbf(path = "mvp_shapefiles/mvp_moorea_confidence_benthic.dbf", simplify_classes = T)
+moorea_geomorphic <- read_dbf("mvp_shapefiles/mvp_moorea_confidence_geomorphic.dbf", simplify_classes = T)
 
 ## KARIMUNJAWA
-kari_benthic <- read_dbf(path = "mvp_karimunjawa_confidence/mvp_karimunjawa_confidence_benthic.dbf", simplify_classes = T)
-kari_geomorphic <- read_dbf(path = "mvp_karimunjawa_confidence/mvp_karimunjawa_confidence_geomorphic.dbf", simplify_classes = T)
+kari_benthic <- read_dbf(path = "mvp_shapefiles/mvp_karimunjawa_confidence_benthic.dbf", simplify_classes = T)
+kari_geomorphic <- read_dbf(path = "mvp_shapefiles/mvp_karimunjawa_confidence_geomorphic.dbf", simplify_classes = T)
 
 ## HAWAI'I
-hawaii_benthic <- read_dbf(path = "mvp_hawaii_confidence/mvp_hawaii_confidence_benthic.dbf", simplify_classes = T)
-hawaii_geomorphic <- read_dbf(path = "mvp_hawaii_confidence/mvp_hawaii_confidence_geomorphic.dbf", simplify_classes = T)
+hawaii_benthic <- read_dbf(path = "mvp_shapefiles/mvp_hawaii_confidence_benthic.dbf", simplify_classes = T)
+hawaii_geomorphic <- read_dbf(path = "mvp_shapefiles/mvp_hawaii_confidence_geomorphic.dbf", simplify_classes = T)
 
 
 
