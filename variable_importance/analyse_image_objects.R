6081f1845d3668ef309fd8901a22a8f78e9bd8c1
KITE\Mitch <mitchell.lyons@unsw.edu.au>
2018-10-31 11:18:23 +1100

    new plot for report

--- variable_importance/analyse_image_objects.R
+++ variable_importance/analyse_image_objects.R
@@ -17,6 +17,21 @@ ggplot(ee_data_plot, aes(y = value, x = class)) +
   geom_boxplot(aes(fill = class)) +
   facet_wrap(~variable, scales = "free")
 
+ee_data_plot <- ee_data_plot[!grepl("_1", ee_data_plot$variable),]
+
+ggplot(ee_data_plot, aes(y = value, x = class)) +
+  #geom_violin(aes(fill = class)) +
+  geom_boxplot(aes(fill = class)) +
+  theme(axis.text.x = element_blank(),
+        strip.text.y = element_text(face = "bold")) +
+  ylab(NULL) +
+  facet_wrap(~variable, scales = "free", ncol = 2,
+             strip.position = "top",
+             labeller = as_labeller(c(b1 = "Coastal Blue", b2 = "Blue",
+                                      b3 = "Green", b4 = "Red", b5 = "NIR",
+                                      depth = "Water Depth (m)", slope = "Slope (deg.)",
+                                      waves = "Sig. wave height (m)")))
+
 # run RF to find threshold characteristics
 rf_fit <- ranger(class ~ b1+b2+b3+b4+b5+depth+slope+waves, data = ee_data, num.trees = 500, importance = "permutation")
 
